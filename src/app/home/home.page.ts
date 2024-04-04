4417d3aee97c1eaa47d9c51bd291f12c73b9acfb
LAPTOP-NELN7LM4\syhuy <huy.t@stor.ai>
2024-04-04 16:08:03 +0700

    css

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { products } from 'src/product-list';
+import { ToastController } from '@ionic/angular';
 register();
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
   product: any[] | undefined;
-  constructor() {
-    this.product = products
-    console.log(this.product);
+  flashLabel: boolean = true;
+  constructor(private toastController: ToastController) {
+    this.product = products;
+  }
+
+  ngOnInit() {
+    this.welcomeToast();
+  }
+
+  welcomeToast() {
+    this.toastCus();
+  }
+  
+  async toastCus() {
+    const toast = await this.toastController.create({
+      message: `Công ty CNK - Xin kính chào quý khách, chúc quý khách một ngày tốt lành !`,
+      duration: 3500,
+      position: 'middle',
+      color: 'success'
+    });
+
+    toast.present();
   }
 }
